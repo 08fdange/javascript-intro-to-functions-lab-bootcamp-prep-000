914a3f3c32a620471b6e1d0c355c1a61ee9d52bb
Frank  D'Angelo <08fdange@gmail.com>
2020-01-21 23:35:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,4 +24,10 @@ function sayHiToGrandma(string) {
   if (whisper(string)) {
     return("I can\'t hear you!")
   }
+  if (string === "I love you, Grandma.") {
+    return("I love you, too.")
+  }
+  else (string) {
+    return("Not working")
+  }
 }
