98ef339ba4f0fd039845dfb9c3ccebdcfebe150b
Frank  D'Angelo <08fdange@gmail.com>
2020-01-22 00:20:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout("hello") {
-  console.log("hello".shout()) 
+function logShout(string) {
+  console.log(string.shout()) 
 }
   
-function logWhisper('hello') {
-  console.log('hello'.whisper())
+function logWhisper(string) {
+  console.log(string.whisper())
 }
 
-
+logShout("hello")
+logWhisper("HELLO")
 
 function sayHiToGrandma(string) {
   if (string === "HELLO") {
