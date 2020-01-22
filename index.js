5a79358916473c3ae9b45a91c5faba3eb1048dc3
Frank  D'Angelo <08fdange@gmail.com>
2020-01-22 00:21:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.shout()) 
+  console.log(string.toUpperCase()) 
 }
   
 function logWhisper(string) {
-  console.log(string.whisper())
+  console.log(string.toLowerCase())
 }
 
 logShout("hello")
